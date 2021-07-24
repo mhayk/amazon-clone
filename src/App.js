14a43d99c536e869a3b372565dc8569876080286
Mhayk Whandson <hi@mhayk.com>
2021-07-24 14:53:02 +0100

    Header component has been imported.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello, amazon clone 🚀!</h1>
+      <Header />
+      {/* Home */}
     </div>
   );
 }
