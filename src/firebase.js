1e25dee1579244edde6c534d6e7dd5b9e0e0093e
Mhayk Whandson <hi@mhayk.com>
2021-07-24 13:47:26 +0100

    Some unecessary files have been deleted.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBX8895MdF1hgVmKzkcCuZiEJlcOywH_DI",
+    authDomain: "fir-a1242.firebaseapp.com",
+    projectId: "fir-a1242",
+    storageBucket: "fir-a1242.appspot.com",
+    messagingSenderId: "532678549316",
+    appId: "1:532678549316:web:375d4b74c71159fc188111",
+    measurementId: "G-JCKT892DP6"
+  };
